7c320ddea1c2113e98f76d18926fc9fd00ca9cbe
senorsec <loukonkenhout@hotmail.com>
2021-02-27 20:00:01 +0100

    super basic setup

--- src/client.c
+++ src/client.c
@@ -2,17 +2,37 @@
 
 #include "./client.h"
 
+/* usage */
+void print_usage(char *cmd){
+	printf("Usage:\n");
+	printf("\t%s -a [IP] -p [PORT]\n", cmd);
+}
 
 /* parse options */
 int parse_opts(int argc, char **argv){
-	while(){
-		
+	int ret = 0, option;
+	while((option = getopt(argc, argv, "a:p:")) != -1){
+		switch(option){
+			case 'a':
+				printf("optarg for a: %s\n", optarg);
+				break;
+			case 'p':
+				printf("optarg for p: %s\n", optarg);
+				break;
+			default:
+				print_usage(argv[0]);
+				ret = -1;
+				break;
+		}
 	}
+	return ret;
 }
 
 
 
 int main(int argc, char **argv){
-	printf("PEARS client");
-	return 0;
+	if(argc < 2){
+		print_usage(argv[0]);
+	}
+	return parse_opts(argc, argv);
 }
