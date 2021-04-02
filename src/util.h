b030749ca3b3755e228daa11c3ef7cd232b532e5
senorsec <loukonkenhout@hotmail.com>
2021-04-02 11:28:24 -0700

    add some headers/macros

--- src/util.h
+++ src/util.h
@@ -4,10 +4,34 @@
 
 #include <getopt.h>
 
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+//#include <netinet/in.h>
+//#include <sys/socket.h>
+
 #include <rdma/rdma_cma.h>
 #include <infiniband/verbs.h>
 
+#define PEARS_DEBUG 1
+
+/* debugging macros */
+#define get_errno() (errno == 0 ? "None" : strerror(errno))
+
+#define log_err(msg, ...) fprintf(stderr, "%s:%d:%s(): \n\terror (errno %d: %s): " msg "\n",\
+				__FILE__, __LINE__, __func__, errno, get_errno(), ##__VA_ARGS__)
+
+
+#ifdef PEARS_DEBUG
+#define debug(msg, ...) fprintf(stdout, "[DEBUG] " msg, ##__VA_ARGS__);
+#else
+#define debug(msg, ...)
+#endif
 
 
 
 #endif
+
+
+
