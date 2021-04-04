e5cd2b6a7a363c3fbf8128817d43970fed99c33e
senorsec <loukonkenhout@hotmail.com>
2021-04-04 14:11:31 -0700

    add file as argument to the script

--- run_client.sh
+++ run_client.sh
@@ -1,21 +1,22 @@
 
-
+#infile=/home/rdmabox/Documents/bsc-project-rdma/testin
+infile=testin
 if [ $# -eq 0 ]
 then
 	ip=127.0.0.1
 	port=20838
 	echo "Using default params: IP $ip PORT $port"
-	./bin/pears_client -a $ip -p $port
+	./bin/pears_client -a $ip -p $port -i $infile
 elif [ $# -eq 1 ]
 then
     if [ $1 == "-vg" ]
     then
         port=20838
         echo "Using default params: IP 0.0.0.0 PORT $port, running with valgrind"
-        valgrind --leak-check=full --track-origins=yes ./bin/pears_client -a 127.0.0.1 -p $port
+        valgrind --leak-check=full --track-origins=yes ./bin/pears_client -a 127.0.0.1 -p $port -i $infile
     fi
 elif [ $# -eq 2 ]
 then
 	echo "Using user params: IP $1 PORT $2"
-	./bin/pears_client -a $1 -p $2
+	./bin/pears_client -a $1 -p $2 -i $infile
 fi
