dd7d868103fbf081a25a0279649a9a5d87d60c49
senorsec <loukonkenhout@hotmail.com>
2021-04-23 21:10:59 +0200

    add variable to control number of requests

--- run_client.sh
+++ run_client.sh
@@ -1,18 +1,18 @@
-
+count=10000000
 infile=input.in
 if [ $# -eq 0 ]
 then
 	ip=10.149.0.53
 	port=5023
 	echo "Using default params: IP $ip PORT $port"
-	./bin/pears_client -a $ip -p $port -i $infile
+	./bin/pears_client -a $ip -p $port -i $infile -c $count
 elif [ $# -eq 1 ]
 then
         port=20838
         echo "Using PORT $port, connecting to node $1"
-        ./bin/pears_client -a 10.149.0.$1 -p $port -i $infile
+        ./bin/pears_client -a 10.149.0.$1 -p $port -i $infile -c $count
 elif [ $# -eq 2 ]
 then
 	echo "Using user params: node $1 PORT $2"
-	./bin/pears_client -a 10.149.0.$1 -p $2 -i $infile
+	./bin/pears_client -a 10.149.0.$1 -p $2 -i $infile -c $count
 fi
