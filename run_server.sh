ce7a308d10f2700c562b44a5e372acb24a8f4344
senorsec <loukonkenhout@hotmail.com>
2021-04-23 20:59:23 +0200

    add option for port

--- run_server.sh
+++ run_server.sh
@@ -1,19 +1,15 @@
 
 
+ip=`ifconfig ib0 | grep "inet " | awk '{print $2}'`
 if [ $# -eq 0 ]
 then
-	ip=`ifconfig ib0 | grep "inet " | awk '{print $2}'`
 	port=20838
 	echo "Using default params: IP $ip PORT $port"
 	./bin/pears_server -a $ip -p $port
 elif [ $# -eq 1 ]
 then
-	if [ $1 == "-vg" ]
-	then
-		port=20838
-		echo "Using default params: IP 0.0.0.0 PORT $port, running with valgrind"
-		valgrind --leak-check=full --track-origins=yes ./bin/pears_server -a 0.0.0.0 -p $port
-	fi
+	echo "Using default params: IP 0.0.0.0 PORT $1, running with valgrind"
+	./bin/pears_server -a $ip -p $1
 elif [ $# -eq 2 ]
 then
 	echo "Using user params: IP $1 PORT $2"
