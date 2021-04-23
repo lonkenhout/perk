ea74a197026e51ee3b851ce024a492b17fbf6304
senorsec <loukonkenhout@hotmail.com>
2021-04-23 20:46:58 +0200

    change param 1 to node number

--- run_client.sh
+++ run_client.sh
@@ -1,22 +1,18 @@
 
-#infile=/home/rdmabox/Documents/bsc-project-rdma/testin
 infile=input.in
 if [ $# -eq 0 ]
 then
-	ip=127.0.0.1
-	port=20838
+	ip=10.149.0.53
+	port=5023
 	echo "Using default params: IP $ip PORT $port"
 	./bin/pears_client -a $ip -p $port -i $infile
 elif [ $# -eq 1 ]
 then
-    if [ $1 == "-vg" ]
-    then
         port=20838
-        echo "Using default params: IP 0.0.0.0 PORT $port, running with valgrind"
-        valgrind --leak-check=full --track-origins=yes ./bin/pears_client -a 127.0.0.1 -p $port -i $infile
-    fi
+        echo "Using PORT $port, connecting to node $1"
+        ./bin/pears_client -a 10.149.0.$1 -p $port -i $infile
 elif [ $# -eq 2 ]
 then
-	echo "Using user params: IP $1 PORT $2"
-	./bin/pears_client -a $1 -p $2 -i $infile
+	echo "Using user params: node $1 PORT $2"
+	./bin/pears_client -a 10.149.0.$1 -p $2 -i $infile
 fi
