33d0156b01a1b9d082ba80bed7173360332e3220
senorsec <loukonkenhout@hotmail.com>
2021-06-20 22:26:45 +0200

    fix memcached kill

--- util/kill_server.py
+++ util/kill_server.py
@@ -3,7 +3,7 @@ import subprocess
 
 def main(argv):
 	if argv[-1] == "memcached":
-		cmd = f"ps=`ps aux`; pid=`echo \"$ps\" | grep {argv[-1]} | awk 'FNR == 3 {{print $2}}'`; echo \"killing $pid\"; kill \"$pid\""
+		cmd = f"ps=`ps aux`; pid=`echo \"$ps\" | grep {argv[-1]} | awk 'FNR == 1 {{print $2}}'`; echo \"killing $pid\"; kill \"$pid\""
 	elif argv[-1] == "memcachedp":
 		cmd = f"ps=`ps aux`; pid=`echo \"$ps\" | grep memcached | awk 'FNR == 4 {{print $2}}'`; echo \"killing $pid\"; kill \"$pid\""
 	else:
